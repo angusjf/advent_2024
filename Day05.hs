d77781991ee211ed0270bd233f486ce7e751b39d
Angus Findlay <angus@humaans.io>
2024-12-05 11:22:06 +0000

    day05

--- Day05.hs
+++ Day05.hs
@@ -1,41 +1,36 @@
-import qualified Data.Map as M
-import Data.List (find)
+import Data.List (find, findIndex, sortBy)
+import Data.Maybe (fromMaybe)
+import Data.Set qualified as S
 
 -- main = readFile "test05.txt" >>= print . solve . parse
+
 main = readFile "input05.txt" >>= print . solve . parse
 
 parse input = (map parseRule rules, map parseUpdate updates)
-    where (rules, "":updates) = break (== "") (lines input)
+  where
+    (rules, "" : updates) = break (== "") (lines input)
 
-parseRule :: String -> (Int, Int)
-parseRule input = (read a, read b)
-  where (a, '|':b) = break (== '|') input
+parseRule input = (a, b)
+  where
+    (a, '|' : b) = break (== '|') input
 
-parseUpdate :: String -> [Int]
-parseUpdate = map read . words . map (\c -> if c == ',' then ' ' else c)
+parseUpdate = words . map (\c -> if c == ',' then ' ' else c)
 
--- solve :: ([(Int, Int)], [[Int]]) -> Int
-solve (rules, updates) = sum $ map middle $ map (\update -> applyRepeatedly pass update rules) $ filter (\update -> not (all (passes update) rules)) updates
+solve (rules, updates) =
+  sum $
+    map read $
+      map middle $
+        map (pass rules) $
+          filter (fails rules) updates
 
-applyRepeatedly makepass update [] = update
-applyRepeatedly makepass update (rule:more) = applyRepeatedly makepass (makepass update rule) more
-
-passes :: [Int] -> (Int, Int) -> Bool
-passes update (before, after) =
-    case (M.lookup before order, M.lookup after order) of
-      (Just x, Just y) -> x < y
-      _ -> True
-  where order = M.fromList (zip update [1..])
-
-middle :: [Int] -> Int
 middle xs = xs !! ((length xs) `div` 2)
 
-pass :: [Int] -> (Int, Int) -> [Int]
-pass [] _ = []
-pass (x:xs) (a, b) = 
-  if x == b then
-    case find (\(_, item) -> item == a) (zip [1..] xs) of
-      Just (i, y) -> pass ((take i xs) ++ [x] ++ (drop i xs)) (a, b)
-      Nothing -> x : (pass xs (a, b))
-  else
-    x : (pass xs (a, b))
+pass :: [(String, String)] -> [String] -> [String]
+pass rules update = sortBy (\a b -> if S.member (a, b) rulesSet then LT else GT) update
+  where
+    rulesSet = S.fromList rules
+
+fails :: [(String, String)] -> [String] -> Bool
+fails rules update = any (\(before, after) -> fromMaybe (-1) (indexOf before update) > fromMaybe 99 (indexOf after update)) rules
+
+indexOf a xs = findIndex (== a) xs
