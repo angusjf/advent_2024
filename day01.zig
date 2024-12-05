7b60aac151b94844c36430737f6fc637670306e8
Angus Findlay <angus@humaans.io>
2024-12-05 11:22:17 +0000

    zig no leaks

--- day01.zig
+++ day01.zig
@@ -8,11 +8,14 @@ fn solve(input: []const u8) !u32 {
     var lines = std.mem.split(u8, input, "\n");
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.detectLeaks();
 
     const alloc = gpa.allocator();
 
     var lefts = std.ArrayList(u32).init(alloc);
+    defer lefts.deinit();
     var rights = std.AutoHashMap(u32, void).init(alloc);
+    defer rights.deinit();
 
     while (lines.next()) |line| {
         if (line.len == 0) break;
