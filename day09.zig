382cd146865b8d470b46d71016e5b71a678788ae
Angus Findlay <angus@humaans.io>
2024-12-09 13:30:06 +0000

    refactor 09

--- day09.zig
+++ day09.zig
@@ -2,18 +2,17 @@ const std = @import("std");
 
 pub fn main() !void {
     std.debug.print("{d}\n", .{try solve(@embedFile("input09.txt"))});
-    //// std.debug.print("{d}\n", .{try solve(@embedFile("test09.txt"))});
 }
 
 fn solve(input: []const u8) !u64 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.detectLeaks();
 
     const alloc = gpa.allocator();
 
-    var ns = std.ArrayList(?u32).init(alloc);
+    var disk = std.ArrayList(?u32).init(alloc);
+    defer disk.deinit();
 
-    // 2333133121414131402
-    // 00 ... 111 ... 2 ... 333.44 . 5555.6666 . 777.888899
     {
         var i: u32 = 0;
         var empty = false;
@@ -22,7 +21,7 @@ fn solve(input: []const u8) !u64 {
             if (c == 10) break;
             const cc: u8 = c - '0';
             for (0..cc) |_| {
-                try ns.append(if (empty) null else i);
+                try disk.append(if (empty) null else i);
             }
             if (!empty) {
                 i += 1;
@@ -31,63 +30,55 @@ fn solve(input: []const u8) !u64 {
         }
     }
 
-    var empty_ptr: usize = 0;
+    try defrag(disk.items);
 
-    while (ns.items[empty_ptr] != null) {
-        empty_ptr += 1;
-    }
+    return checksum(disk.items);
+}
 
-    var right_ptr: usize = ns.items.len - 1;
+fn defrag(disk: []?u32) !void {
+    var right_ptr: usize = disk.len - 1;
 
-    while (ns.items[right_ptr] == null) {
+    while (disk[right_ptr] == null) {
         right_ptr -= 1;
     }
 
-    var id = ns.items[right_ptr].?;
+    var id = disk[right_ptr].?;
 
     id_loop: while (true) {
-        //std.debug.print("id={any}\n", .{id});
-
-        while (ns.items[right_ptr] != id) {
+        while (disk[right_ptr] != id) {
             right_ptr -= 1;
         }
         var len: u32 = 0;
-        while (len <= right_ptr and right_ptr - len >= 0 and ns.items[right_ptr - len] == id) {
+        while (len <= right_ptr and right_ptr - len >= 0 and disk[right_ptr - len] == id) {
             len += 1;
         }
 
         var cursor: usize = 0;
 
         while (true) {
-            //std.debug.print("  looking for gap with size len={any} cursor={any}\n", .{ len, cursor });
-            if (ns.items[cursor] == id) {
-                //std.debug.print("  went too far cursor={any} id={any}\n", .{ cursor, id });
-
+            if (disk[cursor] == id) {
                 break;
             }
 
             var gap_size: usize = 0;
 
-            while (ns.items[cursor + gap_size] == null) {
+            while (disk[cursor + gap_size] == null) {
                 gap_size += 1;
-                //std.debug.print("    found gap gap_size={any}\n", .{gap_size});
-                if (cursor + gap_size + 2 >= ns.items.len) {
+                if (cursor + gap_size + 2 >= disk.len) {
                     id -= 1;
                     continue :id_loop;
                 }
             }
 
             if (gap_size >= len) {
-                //std.debug.print("   found gap size={any} from={any}\n", .{ gap_size, cursor });
-
                 for (0..len) |i| {
-                    ns.items[cursor + i] = ns.items[right_ptr - i];
-                    ns.items[right_ptr - i] = null;
+                    disk[cursor + i] = disk[right_ptr - i];
+                    disk[right_ptr - i] = null;
                 }
                 break;
             } else {
                 cursor += 1;
-                if (cursor + gap_size + 2 >= ns.items.len) {
+                if (cursor + gap_size + 2 >= disk.len) {
                     if (id == 0) {
                         break :id_loop;
                     } else {
@@ -98,26 +89,26 @@ fn solve(input: []const u8) !u64 {
             }
         }
 
-        //std.debug.print("  {any}\n", .{ns.items});
-
         if (id == 0) {
             break;
         } else {
             id -= 1;
         }
     }
-
-    return checksum(ns.items);
 }
 
-fn checksum(ns: []?u32) u64 {
+fn checksum(disk: []?u32) u64 {
     var result: u64 = 0;
 
-    for (0.., ns) |i, n| {
+    for (0.., disk) |i, n| {
         if (n) |nv| {
             result += i * nv;
         }
     }
 
     return result;
+}
+
+test "day09" {
+    try std.testing.expect(try solve("2333133121414131402") == 2858);
 }
