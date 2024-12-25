5637282e17cc6d0348edcbde19ec487ddbda7795
Angus Findlay <angus@humaans.io>
2024-12-25 17:00:39 +0000

    day15 pt2

--- Day15.hs
+++ Day15.hs
@@ -1,58 +1,38 @@
 import Data.List (find)
 import Data.Map qualified as M
 import Data.Maybe
+import Debug.Trace (trace)
 
-main = readFile "test15.txt" >>= print . solve . parse
+main = readFile "input15.txt" >>= print . solve . parse
 
--- main = readFile "test15.txt" >>= putStrLn . vis . parse
-
--- main = readFile "input15.txt" >>= print . solve . parse
-
-dir '^' = (0, -1)
-dir '>' = (1, 0)
-dir '<' = (-1, 0)
-dir 'v' = (0, 1)
+c2dir '^' = (0, -1)
+c2dir '>' = (1, 0)
+c2dir '<' = (-1, 0)
+c2dir 'v' = (0, 1)
 
 move (dx, dy) (x, y) = (x + dx, y + dy)
 
-draw (mx, my) grid =
-  ((('+' :) $ take mx $ repeat '-') ++ "+")
-    : [ '|'
-          : [ grid M.! (x, y)
-            | x <- [0 .. mx - 1]
-            ]
-          ++ ['|']
-      | y <- [0 .. my - 1]
-      ]
-    ++ [(('+' :) $ take mx $ repeat '-') ++ "+"]
+swap :: (Ord k) => k -> k -> M.Map k a -> M.Map k a
+swap p1 p2 dict = M.insert p2 (dict M.! p1) (M.insert p1 (dict M.! p2) dict)
 
 toGrid xs = do
   (y, row) <- zip [0 ..] xs
   (x, c) <- zip [0 ..] row
-  return ((x, y), c)
+  case c of
+    'O' -> [((2 * x, y), '['), ((2 * x + 1, y), ']')]
+    '@' -> [((2 * x, y), '@'), ((2 * x + 1, y), '.')]
+    _ -> [((2 * x, y), c), ((2 * x + 1, y), c)]
 
-parse input = (M.fromList grid, concat ins, start)
+parse input = (M.insert start '.' $ M.fromList grid, concat ins, start)
   where
     (warehouse, ins) = break null $ lines input
     grid = toGrid warehouse
     Just (start, '@') = find ((== '@') . snd) grid
 
 solve (warehouse, ins, start) =
-  sum $
-    map (\(x, y) -> x + y * 100) $
-      map fst $
-        filter ((== 'O') . snd) $
-          -- unlines $
-          --   draw (8, 8) $
-          M.toList $
-            fst $
-              foldl step (warehouse, start) (map dir ins)
-
-vis (warehouse, ins, start) =
-  unlines $
-    draw (8, 8) $
-      fst $
-        foldl step (warehouse, start) (map dir ins)
+  score $ foldl step (warehouse, start) $ map c2dir ins
+  where
+    score = sum . map (\(x, y) -> x + y * 100) . map fst . filter ((== '[') . snd) . M.toList . fst
 
 step :: (M.Map (Int, Int) Char, (Int, Int)) -> (Int, Int) -> (M.Map (Int, Int) Char, (Int, Int))
 step (warehouse, pos) dir =
@@ -61,38 +41,40 @@ step (warehouse, pos) dir =
         '#' -> (warehouse, pos)
         '.' -> (warehouse, pos')
         '@' -> (warehouse, pos')
-        'O' ->
-          case dir of
-            (0, 1) ->
-              case pushLeft warehouse pos of
-                Nothing -> (warehouse, pos)
-                Just warehouse' -> (warehouse', left pos)
+        '[' ->
+          fromMaybe (warehouse, pos) $
+            case dir of
+              (1, 0) -> (,pos') <$> pushX pos dir warehouse
+              (-1, 0) -> error "[<"
+              _ -> (,pos') <$> pushY pos dir warehouse
+        ']' ->
+          fromMaybe (warehouse, pos) $
+            case dir of
+              (1, 0) -> error "]<"
+              (-1, 0) -> (,pos') <$> pushX pos dir warehouse
+              _ -> (,pos') <$> pushY pos dir warehouse
 
-left (x, y) = (x - 1, y)
+pushX :: (Int, Int) -> (Int, Int) -> M.Map (Int, Int) Char -> Maybe (M.Map (Int, Int) Char)
+pushX pos dir warehouse =
+  let pos' = move dir pos
+   in case M.lookup pos' warehouse of
+        Just '.' -> Just $ swap pos pos' warehouse
+        Just '#' -> Nothing
+        Just c | c `elem` "[]" -> swap pos pos' <$> pushX pos' dir warehouse
+        Nothing -> Nothing
 
-swap :: (Ord k) => k -> k -> M.Map k a -> M.Map k a
-swap p1 p2 dict = M.insert p2 (dict M.! p1) (M.insert p1 (dict M.! p2) dict)
-
-pushLeft :: M.Map (Int, Int) Char -> (Int, Int) -> Maybe (M.Map (Int, Int) Char)
-pushLeft warehouse (x, y) =
-  case warehouse M.! (x - 1, y) of
-    '.' -> Just (swap (x, y) (x - 1, y) warehouse)
-    ']' ->
-      pushLeft warehouse (x - 2, y) >>= \warehouse' -> Just (swap (x, y) (x - 1, y) warehouse')
-    '#' ->
-      Nothing
-
-pushUp :: M.Map (Int, Int) Char -> (Int, Int) -> Maybe (M.Map (Int, Int) Char)
-pushUp warehouse (x, y) =
-  case warehouse M.! (x, y - 1) of
-    '.' -> Just (swap (x, y) (x, y - 1) warehouse)
-    ']' ->
-      pushUp warehouse (x - 2, y) >>= \warehouse' -> Just (swap (x, y) (x - 1, y) warehouse')
-    '#' -> Nothing
-
--- findEmptySpace warehouse dir pos =
---   case warehouse M.! pos of
---     '.' -> Just pos
---     '@' -> Just pos
---     '#' -> Nothing
---     'O' -> findEmptySpace warehouse dir (move dir pos)
+pushY :: (Int, Int) -> (Int, Int) -> M.Map (Int, Int) Char -> Maybe (M.Map (Int, Int) Char)
+pushY pos dir warehouse =
+  let pos' = move dir pos
+   in case M.lookup pos' warehouse of
+        Just '.' -> Just $ swap pos pos' warehouse
+        Just '#' -> Nothing
+        Nothing -> Nothing
+        Just ']' -> do
+          w' <- pushY pos' dir warehouse
+          w'' <- pushY (move (c2dir '<') pos') dir w'
+          return $ swap pos pos' w''
+        Just '[' -> do
+          w' <- pushY pos' dir warehouse
+          w'' <- pushY (move (c2dir '>') pos') dir w'
+          return $ swap pos pos' w''
