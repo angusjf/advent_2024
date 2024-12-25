3b0fce337b2d903ef50af399a36977fd840265b1
Angus Findlay <angus@humaans.io>
2024-12-25 12:29:24 +0000

    day12 pt2

--- Day12.hs
+++ Day12.hs
@@ -1,14 +1,15 @@
 import Data.Function (on)
-import Data.List (groupBy, intersperse, maximumBy, minimumBy, nub, sortBy)
+import Data.List (groupBy, intersperse, maximumBy, minimumBy, nub, partition, singleton, sortBy)
 import Data.Map ((!))
 import Data.Map qualified as M
 import Data.Maybe (catMaybes)
 import Data.Ord (comparing)
 import Data.Set qualified as S
+import Debug.Trace (trace)
 
--- main = readFile "input12.txt" >>= print . solve . parse
+main = readFile "input12.txt" >>= print . solve . parse
 
-main = readFile "test12.txt" >>= putStrLn . solve . parse
+-- main = readFile "test12.txt" >>= print . solve . parse
 
 parse = toGrid . lines
 
@@ -18,10 +19,28 @@ toGrid xs = do
   return ((x, y), c)
 
 solve points =
-  -- sum $
-  unlines $ map score $ zones grid
+  sum
+    $ map
+      ( \(_, points) ->
+          let area = length points
+              perimeter = length $ concat $ M.elems $ perim points
+           in area * sides points
+      )
+    $ zones
+    $ M.fromList points
+
+groupAdj :: S.Set (Int, Int) -> [S.Set (Int, Int)]
+groupAdj all =
+  case S.minView all of
+    Just (x, _) ->
+      let adj = allAdjacent (S.singleton x) all
+       in adj : groupAdj (all S.\\ adj)
+    Nothing -> []
+
+sides points = sum $ map (length . groupAdj . S.fromList) $ M.elems p
   where
-    grid = M.fromList points
+    p = perim points
+    adj (x0, y0) (x1, y1) = x0 == x1 || y0 == y1
 
 zones :: M.Map (Int, Int) Char -> [(Char, S.Set (Int, Int))]
 zones grid =
@@ -31,7 +50,7 @@ zones grid =
        in (char, positions) : zones (M.withoutKeys grid positions)
     Nothing -> []
 
-neighbors (x, y) = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 zone :: M.Map (Int, Int) Char -> ((Int, Int), Char) -> S.Set (Int, Int)
 zone grid (pos, c) = allAdjacent (S.singleton pos) (S.fromList $ map fst $ filter ((== c) . snd) $ M.toList grid)
@@ -42,40 +61,21 @@ allAdjacent ok toCheck =
     (new, old) | S.null new || S.null old -> S.union ok new
     (new, old) -> allAdjacent (S.union ok new) old
 
+(x, y) +: (a, b) = (x + a, b + y)
+
 isAdjacentToAny ok pos = any (\pos -> S.member pos ok) (neighbors pos)
+  where
+    neighbors pos = map (+: pos) dirs
 
-draw (mx, my) grid =
-  ((('+' :) $ take mx $ repeat '-') ++ "+")
-    : [ '|'
-          : [ if S.member (x, y) grid then '#' else ' '
-            | x <- [-1 .. mx - 1]
-            ]
-          ++ ['|']
-      | y <- [-2 .. my - 2]
-      ]
-    ++ [(('+' :) $ take mx $ repeat '-') ++ "+"]
+perim :: S.Set (Int, Int) -> M.Map (Int, Int) [(Int, Int)]
+perim points = go [S.findMin points] M.empty S.empty
+  where
+    go :: [(Int, Int)] -> M.Map (Int, Int) [(Int, Int)] -> S.Set (Int, Int) -> M.Map (Int, Int) [(Int, Int)]
+    go [] acc _ = acc
+    go (pos : q) acc visited =
+      let optim xy = xy `notElem` q && S.notMember xy visited
+          onShape xy = S.member xy points
+          (ok, ko) = partition (onShape . snd) $ filter (optim . snd) $ zipWith (\p v -> (v, p +: v)) (repeat pos) dirs
 
--- score :: (Char, S.Set (Int, Int)) -> ?
-score (c, points) = [c] ++ ":\n" ++ (unlines $ draw (11, 11) $ nEdges points)
-
--- (c, length points, nEdges points)
-
-nEdges points = convolve2x2Map (10, 10) leftEdgesKernel points
-
-leftEdgesKernel = S.fromList [(0, 0), (1, 0)]
-
-convolve2x2Map :: (Int, Int) -> S.Set (Int, Int) -> S.Set (Int, Int) -> S.Set (Int, Int)
-convolve2x2Map (maxX, maxY) kernel matrix =
-  S.fromList $
-    catMaybes
-      [ if any
-          id
-          [ S.member (kx, ky) kernel && S.member (x + kx, y + ky) matrix
-          | kx <- [0, 1],
-            ky <- [0, 1]
-          ]
-          then Just (x, y)
-          else Nothing
-      | x <- [-2 .. maxX + 1],
-        y <- [-2 .. maxY + 1]
-      ]
+          acc' = M.unionWith (++) acc $ M.fromListWith (++) $ map (\(k, v) -> (k, [v])) ko
+       in go (q ++ map snd ok) acc' (S.insert pos visited)
